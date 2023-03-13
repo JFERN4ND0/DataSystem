68340b546e1393b408cfe0c501cae0d11da3cc0f
FERNANDO <joferguzvas@gmail.com>
2023-03-13 03:27:55 -0600

    Systema de Geekipedia de Hernesto

---
+++ db_ds.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2022 a las 13:04:04
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_ds`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `mail_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tel_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dir_cliente` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `ultima_modificacion` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre_cliente`, `mail_cliente`, `tel_cliente`, `dir_cliente`, `ultima_modificacion`) VALUES
+(1, 'AngelGarcia', 'ag@ejemplo.com', '555555555', 'Avenida de las flores', 'juan_capturista'),
+(2, 'Roberto Alvarez', 'ra@ejemplo.com', '9514670474', 'Avenida alta #54', 'juan_capturista');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `id_equipo` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `tipo_equipo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `marca` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `modelo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `num_serie` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dia_ingreso` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `mes_ingreso` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `annio_ingreso` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `observaciones` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ultima_modificacion` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `comentarios_tecnicos` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `revicion_tecnica` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Es una tabla para registrar equipos';
+
+--
+-- Volcado de datos para la tabla `equipos`
+--
+
+INSERT INTO `equipos` (`id_equipo`, `id_cliente`, `tipo_equipo`, `marca`, `modelo`, `num_serie`, `dia_ingreso`, `mes_ingreso`, `annio_ingreso`, `observaciones`, `status`, `ultima_modificacion`, `comentarios_tecnicos`, `revicion_tecnica`) VALUES
+(1, 1, 'Multifuncional', 'Brother', 'MFC9990', 'u65413', '31', '2', '2022', 'No enciende', 'En revision', 'juan_capturista', 'Falta el cable de corriente', 'gerardo_tecnico'),
+(2, 2, 'Laptop', 'Acer', '5589', 'u987456', '31', '2', '2022', 'No enciende', 'Nuevo ingreso', 'juan_capturista', '', ''),
+(3, 2, 'Impresora', 'HP', '1100', 'u965412', '31', '2', '2022', 'Sin observaciones.', 'Nuevo ingreso', 'juan_capturista', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tipo_nivel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `estatus` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `registrado_por` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `email`, `telefono`, `username`, `password`, `tipo_nivel`, `estatus`, `registrado_por`) VALUES
+(1, 'Fernando G', 'Fernando@ejem.com', '789456123', 'Fernando', '123456', 'Administrador', 'Activo', 'Fernando'),
+(2, 'Juan', 'juan@ejemplo.com', '456789123', 'juan_capturista', '123456', 'Capturista', 'Activo', 'Fernando'),
+(3, 'Gerardo Tellez', 'gerardo@ejemplo.com', '456789123', 'gerardo_tecnico', '123456', 'Tecnico', 'Activo', 'Fernando'),
+(6, 'Jose Guzman', 'ejemplo@ejem.com', '9514670422', 'JoseG', '654321', 'Capturista', 'Activo', 'Fernando'),
+(7, 'Abril Rubi', 'abril@ejemplo', '123456789', 'AbrilR', '123456', 'Administrador', 'Activo', 'Fernando');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`id_equipo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
